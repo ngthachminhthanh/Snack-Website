80e0aa5408c1cf058075685a763886f66411c5ac
Minh Thanh <ngthachminhthanh@gmail.com>
2025-06-27 00:02:26 +0700

    setup deploy

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const connectDB = require("./database");
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());

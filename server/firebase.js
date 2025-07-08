eb5ec343a0e1a6e45fa83beacdf0a7ebbaac6387
Minh Thanh <ngthachminhthanh@gmail.com>
2025-07-08 16:24:00 +0700

    fix google credentials (1)

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,10 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
 
-const serviceAccount = JSON.parse(process.env.CREDENTIALS_FIREBASEADMINSDK);
+// Đọc nội dung JSON từ file
+const serviceAccount = JSON.parse(
+  fs.readFileSync(process.env.CREDENTIALS_FIREBASEADMINSDK, 'utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
